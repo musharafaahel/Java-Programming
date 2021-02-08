55b6672bc8903b28db39ada0d902c770d7976103
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-02-08 20:12:11 +0530

    Day 1 College class

---
+++ P1.java
@@ -0,0 +1,7 @@
+class P1
+{
+ public static void main(String s[])
+ {
+ System.out.println("hello world!");
+ }
+}
---
+++ sum.java
@@ -0,0 +1,11 @@
+class sum
+{
+ public static void main(String s[])
+ {
+  int a = 10, b = 20;
+  int c = a + b;
+  System.out.println("The Given value for variable a is "+ a );
+  System.out.println("The Given value for variable b is "+ b );
+  System.out.println("Sum of a and b is "+ c);
+ }
+}
