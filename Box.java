0927e0783a54906f9e0b18c5a6087e99f380d870
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-03-21 06:59:25 +0530

    Unit 2

---
+++ Box.java
@@ -0,0 +1,12 @@
+//Volume of Box = length*breadth*heigth
+
+class Box
+{
+	public static void main(String args[])
+	{
+		int length = 5;
+		int breadth = 5;
+		int heigth = 5;
+		System.out.println("Volume of Box = "+length*breadth*heigth);	
+	}
+}
