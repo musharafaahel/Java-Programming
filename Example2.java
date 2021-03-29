0043ae119fce880551012454becd83ce25408769
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-03-29 16:48:08 +0530

    Complete Reference Unit 1 Chapter 2

---
+++ Example2.java
@@ -0,0 +1,17 @@
+/*
+	Here is another short example
+	Call this file as "Example.java"
+*/
+
+class Example2
+{
+	public static void main(String args[])
+	{
+		int num;	//variable is declared with name num
+		num = 100;    //value assigned to a variable 100
+		System.out.println("This is num : "+ num);
+		num = num * 2;
+		System.out.print("The value of num * 2 is ");
+		System.out.println(num);
+	}
+}
