417d7aa60f970ab7b3ac7b219024010f24a71228
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-03-29 16:47:20 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,12 @@
+/*This is a simple Java program
+Call this file as "Example.java"
+*/
+
+class Example
+{
+	//Your program begins with call to main
+	public static void main(String args[])
+	{
+		System.out.println("This is a simple Java program.");
+	} 
+}
