db4719ef3ad80db59c6d6222087efa8fc6340464
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-03-30 21:37:29 +0530

    Complete Reference Unit 1 Chapter 3

---
+++ CharDemo.java
@@ -0,0 +1,13 @@
+//Demonstrate char data type
+class CharDemo
+{
+	public static void main(String args[])
+	{
+		char ch1, ch2;
+		ch1 = 88; //code for Y
+		ch2 = 'Y';
+		
+		System.out.println("ch1 and ch2 : " + ch1 + " "+ ch2);
+		
+	}
+}
