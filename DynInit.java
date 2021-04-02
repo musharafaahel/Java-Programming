9c0eb65b2b9cb09b20f67b4c7742eb3546898d1c
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-04-02 22:47:59 +0530

    Complete Reference Unit 1 Chapter 3

---
+++ DynInit.java
@@ -0,0 +1,10 @@
+class DynInit
+{
+	public static void main(String[] args)
+	{
+		double a = 3.0, b = 4.0;
+		
+		double nc = Math.sqrt(a * a + b * b);
+		System.out.println("Hypotenuse : "+ c);
+	}
+}
