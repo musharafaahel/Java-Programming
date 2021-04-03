3a77ee481c4c465b7e1f3e69f7df377ba2a31be8
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-04-03 19:16:53 +0530

    Complete Reference Unit 1 Chapter 3

---
+++ AutoArray.java
@@ -0,0 +1,8 @@
+class AutoArray
+{
+	public static void main(String[] args)
+	{
+		int month_days[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+		System.out.println("October has "+ month_days[9] + " days. ");	
+	}
+}
