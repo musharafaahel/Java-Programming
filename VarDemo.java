35b9df04e2853494c0df232f314a7c17052e50f5
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-04-04 11:16:07 +0530

    Complete Reference Unit 1 Chapter 3

---
+++ VarDemo.java
@@ -0,0 +1,14 @@
+class VarDemo
+{
+	public static void main(String[] args)
+	{
+		var i = 0;
+		System.out.println("Value of i = "+ i);
+		
+		int var = 1;
+		System.out.println("Value of var = "+ var);
+		
+		var j = -var;
+		System.out.println("Value of j ="+ j);
+	}
+}
