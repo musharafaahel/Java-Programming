3a2b35262bb93446a207bbc5c8601f2b7b046ff9
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-04-07 19:18:46 +0530

    Abstract Class & Abstract Method Concept

---
+++ AbstractDemo.java
@@ -0,0 +1,33 @@
+abstract class JetKings
+{
+	abstract int noOfComputers();	
+}
+
+class TNagar extends JetKings
+{
+	int noOfComputers()
+	{
+		return 40;
+	}
+}
+
+class KKNagar extends JetKings
+{
+	int noOfComputers()
+	{
+		return 50;
+	}
+}
+
+class AbstractDemo
+{
+	public static void main(String[] args)
+	{
+		TNagar t = new TNagar();
+		t.noOfComputers();
+		System.out.println("No of Computers at T Nagar Branch : "+ t.noOfComputers());
+		KKNagar k = new KKNagar();
+		k.noOfComputers();
+		System.out.println("No of Computers at KK Nagar Branch : "+ k.noOfComputers());
+	}
+}
