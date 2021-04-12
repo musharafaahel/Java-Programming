e9f141bd69bbf411ddfb410c4fa922af92cc2952
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-04-12 10:19:03 +0530

    OperatorsAndOperand

---
+++ OperatorsAndOperands.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class OperatorsAndOperands {
+    public static void main(String[] args) {
+        int num1 = 55;
+        int num2 = 10;
+
+        System.out.println("num1 + num2 = "+ (num1 + num2));
+        System.out.println("num1 - num2 = "+ (num1 - num2));
+        System.out.println("num1 * num2 = "+ (num1 * num2));
+        System.out.println("num1 / num2 = "+ (num1 / num2));
+        System.out.println("num1 % num2 = "+ (num1 % num2));
+    }
+}
