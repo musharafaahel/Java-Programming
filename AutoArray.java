d8a140589af25b0bf0e8090cacfa455056d61386
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-04-15 10:53:23 +0530

    Update AutoArray.java

--- AutoArray.java
+++ AutoArray.java
@@ -1,6 +1,6 @@
 class AutoArray
 {
-	public static void main(String[] args)
+		public static void main(String[] args)
 	{
 		int month_days[] = {31,28,31,30,31,30,31,31,30,31,30,31};
 		System.out.println("October has "+ month_days[9] + " days. ");	
