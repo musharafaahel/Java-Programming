c87aa992ee26ca0c08e61b9b1733539bd49c4cb6
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-06-03 19:55:08 +0530

    Add files via upload

---
+++ FindLcm.java
@@ -0,0 +1,20 @@
+package com.hit;
+
+public class FindLcm {
+
+	public static void main(String[] args) {
+		int n1 = 16 ,n2 = 20, lcm;
+		
+		lcm = (n1 > n2)? n1 :n2;
+		
+		while(true) {
+			if(lcm % n1 == 0 && lcm % n2 == 0 ) {
+				System.out.printf("The LCM of %d and %d is %d ", n1 , n2, lcm);
+				break;
+			}
+			++lcm;
+		}
+
+	}
+
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,30 @@
-package mypack;
-import mypack.Hello2;
-public class HelloWorld{
-	
+package com.hit;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * 
+		 * Multiline comment
+		 * 
+		 */
+		/**
+		 * 
+		 * Documentation comment
+		 * author: Musharaf Aahel
+		 * Date : 03-06-21
+		 */
+		
+		System.out.println("Welcome to java...........");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String name = scan.next();
+		System.out.println("Welcome to Java....."+name);
+		
+	}
+
 }
