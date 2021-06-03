003ea60cad7edddfbdd4514ab0967682fc68cfc8
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-06-03 19:54:47 +0530

    Add files via upload

---
+++ CheckEvenOdd.java
@@ -0,0 +1,13 @@
+package com.hit;
+
+public class CheckEvenOdd {
+	public static void main(String[] args) {
+		int num = 20;
+		
+		if(num % 2 == 0) {
+			System.out.println("Entered number is Even");
+		} else {
+			System.out.println("Entered number is Odd");
+		}
+	}
+}
---
+++ addition.java
@@ -0,0 +1,19 @@
+package com.hit;
+
+import java.util.Scanner;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10;
+		int b =10;
+
+		int sum = a + b;
+	
+		System.out.println("Addition of two number : " + sum);
+		
+
+	}
+
+}
